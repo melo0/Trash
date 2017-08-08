6d3d2e0369b2a19189f1da5d4dd84ee6cd52a51b
melo0 <melo0@op.pl>
2017-08-08 11:52:58 +0200

    Update data.py

--- data.py
+++ data.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-
 # Program do obliczania liczby dni/tygodni/miesięcy od wpisanej daty do dnia dzisiejszego (lub wprowadzonej ręcznie daty)
 
 
@@ -28,7 +27,6 @@ okno = tkinter.Tk()
 okno.title("Data")
 okno.geometry("300x150")
 
-
 pole_t1 = tkinter.Entry(okno)
 pole_t2 = tkinter.Entry(okno)
 pole_t3 = tkinter.Text(height = 1, width = 20)
@@ -38,7 +36,6 @@ pole_t5 = tkinter.Text(height = 1, width = 20)
 pole_t1.insert(END, "2016-12-16")
 pole_t2.insert(END, time.strftime("%Y-%m-%d"))
 
-
 etykieta1 = tkinter.Label(okno, text = "Data początkowa: ")
 etykieta2 = tkinter.Label(okno, text = "Dzisiejsza data: ")
 etykieta3 = tkinter.Label(okno, text = "Liczba dni: ")
@@ -46,18 +43,17 @@ etykieta4 = tkinter.Label(okno, text = "Liczba tygodni: ")
 etykieta5 = tkinter.Label(okno, text = "Liczba miesięcy: ")
 
 
+pole_t1.grid(row=0, column = 2, stick = W+E)
+pole_t2.grid(row=2, column = 2, stick = W+E)
+pole_t3.grid(row=4, column = 2, stick = W+E)
+pole_t4.grid(row=6, column = 2, stick = W+E)
+pole_t5.grid(row=8, column = 2, stick = W+E)
 
-pole_t1.grid(row=0, column = 2)
-pole_t2.grid(row=2, column = 2)
-pole_t3.grid(row=4, column = 2)
-pole_t4.grid(row=6, column = 2)
-pole_t5.grid(row=8, column = 2)
-
-etykieta1.grid(row=0, column = 1)
-etykieta2.grid(row=2, column = 1)
-etykieta3.grid(row=4, column = 1)
-etykieta4.grid(row=6, column = 1)
-etykieta5.grid(row=8, column = 1)
+etykieta1.grid(row=0, column = 1, stick = W)
+etykieta2.grid(row=2, column = 1, stick = W)
+etykieta3.grid(row=4, column = 1, stick = W)
+etykieta4.grid(row=6, column = 1, stick = W)
+etykieta5.grid(row=8, column = 1, stick = W)
 
 przycisk = tkinter.Button(okno, text = "Oblicz", command = lambda: oblicz() )
 przycisk.grid(row = 11, column = 2)
